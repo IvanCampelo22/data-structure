d083a9f2d29d5f103d511a29790a67a2e3450f2e
Ivan Campelo <64935453+IvanCampelo22@users.noreply.github.com>
2023-07-17 10:34:20 -0300

    feat: convert infixa in posfixa

---
+++ expressões/prosfixa.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "../pilhas/pilha.h"
+
+char *prosfixa(char *e){
+    static char s[256];
+    int j=0;
+    Pilha P = pilha(256);
+    for (int i=0; e[i]; i++)
+        if( isdigit(e[i]) ) s[j++] = e[i];
+        else if(strchr("+*-/", e[i]) ) empilha(e[i], P);
+        else if(e[i] == ')' ) s[j++] = desempilha(P);
+    s[j]='\0';
+    destroip(&P);
+    return s;
+    }
+
+int main(void){
+    char e[256];
+    printf("Infixa?");
+    gets(e);
+    printf("Posfixa: %s\n\n", prosfixa(e));
+    return 0;
+}
+
